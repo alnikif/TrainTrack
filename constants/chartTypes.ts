86f68e524f873f341ed424ef5f547ed3e2441143
alnikif <nikiforiakoleksandr@gmail.com>
2024-02-17 16:49:06 +0100

    change chartType

--- constants/chartTypes.ts
+++ constants/chartTypes.ts
@@ -5,21 +5,6 @@ export enum ChartTypes {
   BarChart = 'bar',
   PieChar = 'pie',
   ScatterPlot = 'scatter',
-  BubbleChart = 'bubble',
-  HeatMap = 'heatmap',
-  PolarChart = 'polar',
-  GaugeChart = 'gauge',
-  BoxPlot = 'boxplot',
-  FunnelChart = 'funnel',
-  PyramidChart = 'pyramid',
-  WaterfallChart = 'waterfall',
-  ErrorBarChart = 'errorbar',
-  CandlestickChart = 'candlestick',
-  OHLCChart = 'ohlc',
-  RadarChart = 'radar',
-  NetworkGraph = 'networkgraph',
-  Treemap = 'treemap',
-  SolidGaugeChart = 'solidgauge',
 }
 
 export interface ChartType {
@@ -27,6 +12,6 @@ export interface ChartType {
   title: string
 }
 
-export const DEFAULT_CHART = ChartTypes.BarChart
+export const DEFAULT_CHART = ChartTypes.ColumnChart
 
 export const chartTypesArray: ChartType[] = Object.entries(ChartTypes).map(([title, key]) => ({ title, key }))
