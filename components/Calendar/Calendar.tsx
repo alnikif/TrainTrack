c0c7ec926912a0fc785b5ad5983b5811abc51a4f
alnikif <nikiforiakoleksandr@gmail.com>
2024-02-20 22:18:31 +0100

    working with DatePicker

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import DatePicker from 'react-datepicker'
 
 import 'react-datepicker/dist/react-datepicker.css'
@@ -6,19 +6,33 @@ import { DateContext } from '../../providers/DateProvider'
 
 const Calendar = () => {
   const { selectedDate, setSelectedDate } = useContext(DateContext)
-  const { endDate } = selectedDate
+  const { startDate, endDate } = selectedDate
+  const [initialDate, setInitialDate] = useState(new Date())
 
   const handleDateChange = (date: Date | null) => {
     if (date) {
-      setSelectedDate((prevState) => ({
-        ...prevState,
-        endDate: date,
-      }))
+      setInitialDate(date)
+    }
+  }
+
+  const onChange = (dates: [Date | null, Date | null]) => {
+    const [start, end] = dates
+    if (start !== null && end !== null) {
+      setSelectedDate({ startDate: start, endDate: end })
     }
   }
 
   return (
-    <DatePicker inline dateFormat="yyyy-MM-dd" selected={endDate} maxDate={new Date()} onChange={handleDateChange} />
+    <DatePicker
+      inline
+      dateFormat="yyyy-MM-dd"
+      startDate={startDate}
+      endDate={endDate}
+      selectsRange
+      selected={initialDate}
+      onChange={onChange}
+      onSelect={handleDateChange}
+    />
   )
 }
 
