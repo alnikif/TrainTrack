23642e974ee51b8f83005a06192ce7b49711dbb2
alnikif <nikiforiakoleksandr@gmail.com>
2024-02-28 17:28:08 +0100

    create training session page

--- constants/navItemsConfig.ts
+++ constants/navItemsConfig.ts
@@ -2,9 +2,11 @@ export enum NavItemsPaths {
   heartRateChart = '/heart-rate-chart',
   stepsCounterChart = '/steps-counter-chart',
   caloriesChart = '/calories-counter-chart',
+  trainingSessionChart = '/training-sessions',
 }
 export const navItemsConfig = [
   { label: 'Heart Rate', url: NavItemsPaths.heartRateChart },
   { label: 'Steps Counter', url: NavItemsPaths.stepsCounterChart },
   { label: 'Calories', url: NavItemsPaths.caloriesChart },
+  { label: 'Training Session', url: NavItemsPaths.trainingSessionChart },
 ]
