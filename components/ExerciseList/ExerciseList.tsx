f5f6db1fc115cb92ed9992c28ef62d8d120d3d52
alnikif <nikiforiakoleksandr@gmail.com>
2024-03-11 22:51:12 +0100

    add styles for training list

--- components/ExerciseList/ExerciseList.tsx
+++ components/ExerciseList/ExerciseList.tsx
@@ -10,11 +10,11 @@ const ExerciseList: FC<ExerciseListPropsType> = ({ exercisesList }) => {
   return (
     <div className="flex">
       {exercisesList?.map((el, index) => (
-        <div className="ml-10 flex flex-col p-2" key={index}>
+        <div className="ml-10 flex flex-col p-3 border border-solid border-black border-1" key={index}>
           <div>{el.title}</div>
           {el.series.map((item, i) => (
             <div key={i}>
-              weight: {item.weight}, reps: {item.reps}
+              weight: {item.weight} kg, reps: {item.reps}
             </div>
           ))}
         </div>
